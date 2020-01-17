e8b29db835197da106b503adca38c8c9540f271b
945hanlin <328507716@qq.com>
2020-01-17 17:57:46 +0800

    实验

---
+++ 2019.11.13/2019.11.13/text.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a, b, c, t;
+		scanf("%d%d%d", &a, &b, &c);
+		if (a > b){
+			t = a;
+			a = b;
+			b = t; 
+		}
+		if (a > c){
+			t = a;
+			a = c;
+			c = t;
+		}
+		if (b > c){
+			t = b;
+			b = c;
+			c = t;
+		}
+		printf("%d%d%d", c, b, a);
+	system("pause");
+	return 0;
+}
